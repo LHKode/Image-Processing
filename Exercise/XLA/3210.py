891bb92d2eb725dc5cddb1fae6ffbf674327504c
lhk1234 <hoangkhanh9119@gmail.com>
2020-10-23 19:49:39 +0700

    go

--- Exercise/XLA/3210.py
+++ Exercise/XLA/3210.py
@@ -40,6 +40,29 @@ g = np.fft.fftshift(np.fft.fft2(img))  # fft and shift to center
 img_apply = g * H  # apply filter
 img_butterworth_lp = abs(np.fft.ifft2(np.fft.ifftshift(img_apply)))
 img_butterworth_lp = np.uint8(img_butterworth_lp)
-cv2.imshow('go',img_butterworth_lp)
-cv2.imshow("o",img)
-cv2.waitKey()
+# cv2.imshow('go',img_butterworth_lp)
+# cv2.imshow("o",img)
+# cv2.waitKey()
+img = cv2.imread('hw4_radiograph_2.jpg',0)
+img_butterworth_lp = cv2.resize(img, (600,600))
+# gauss = np.random.normal(0,1,img.size)
+# gauss = gauss.reshape(img.shape[0], img.shape[1], img.shape[2]).astype('uint8')
+# img_gauss = cv2.add(img, gauss)
+ret,img_binary = cv2.threshold(img_butterworth_lp,127,255,cv2.THRESH_BINARY)
+ret,img_trunc = cv2.threshold(img_butterworth_lp,127,255,cv2.THRESH_TRUNC)
+ret,img_tozero = cv2.threshold(img_butterworth_lp,127,255,cv2.THRESH_TOZERO)
+ret, img_otsu = cv2.threshold(img_butterworth_lp, 0,255, cv2.THRESH_OTSU)
+img_threshold = cv2.adaptiveThreshold(img_butterworth_lp, 255, cv2.ADAPTIVE_THRESH_MEAN_C,\
+                                      cv2.THRESH_BINARY,11,2)
+img_gaussian = cv2.adaptiveThreshold(img_butterworth_lp, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C,\
+                                    cv2.THRESH_BINARY,11,2)
+
+titles = ['Mean', 'Gaussian', 'Tozero', 'Binary', 'Trunc','Otsu']
+images = [img_threshold, img_gaussian, img_tozero, img_binary, img_trunc, img_otsu]
+cv2.imshow('Original', img)
+for i in range(6):
+    plt.subplot(3,3,i+1)
+    plt.imshow(images[i],'gray')
+    plt.title(titles[i])
+    plt.xticks([]),plt.yticks([])
+plt.show()
